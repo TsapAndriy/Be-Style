353a5715b611a59fb8ed7a6851e79320c39c0f3a
TsapAndriy <tsaplemberg@gmail.com>
2015-06-10 08:55:39 +0400

    10-06

--- Clickable/commonScript.js
+++ Clickable/commonScript.js
@@ -12,17 +12,28 @@ function addPicture(item){
 	if (imgPosition){
 		var elem = document.createElement("img");
 		elem.setAttribute("src", item.src);
-		elem.setAttribute("height", 2*item.height);
-		elem.setAttribute("width", 2*item.width);
-		//elem.setAttribute("height", 100);
+		//elem.setAttribute("height", 2*item.height);
+		//elem.setAttribute("width", 2*item.width);
+		//elem.setAttribute("height", imgPosition.clientHeight);
+		//elem.setAttribute("width", imgPosition.clientWidth);
+		
+		if (item.height/item.width > 1){			
+			elem.setAttribute("height", 0.5*imgPosition.clientHeight);
+		}
+		else {
+			elem.setAttribute("width", 0.5*imgPosition.clientWidth);
+		}
+		
+		//elem.style.paddingBottom = '0';
+		//elem.style.marginBottom = 0;
+		
 		elem.addEventListener("click", function(){
-			if (selectedPicture)
+			if (selectedPicture) {
 				selectedPicture.style.border = 'none';
+			}					
 			elem.style.border = '1px solid #E8272C';
-			//elem.style.display = 'block';
-			//elem.style.margin = '0 auto';
 			selectedPicture = elem;
-		});
+			});
 		
 		imgPosition.appendChild(elem);
 	}
